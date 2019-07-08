fc861f6f97a4159051b0c4c83faa22519315be91
Robert Sehr <robert.sehr@intranda.com>
2019-07-08 10:11:33 +0200

    on success, redirect jsf page, on error display error test

--- goobi-plugin-rest-intranda/src/org/goobi/api/rest/Emails.java
+++ goobi-plugin-rest-intranda/src/org/goobi/api/rest/Emails.java
@@ -1,5 +1,6 @@
 package org.goobi.api.rest;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -8,12 +9,16 @@ import java.util.List;
 import java.util.Map;
 
 import javax.naming.ConfigurationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.goobi.api.mail.StepConfiguration;
 import org.goobi.api.mail.UserProjectConfiguration;
@@ -22,12 +27,14 @@ import org.goobi.beans.User;
 import de.sub.goobi.helper.JwtHelper;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.persistence.managers.UserManager;
+import lombok.extern.log4j.Log4j;
 
 /**
  * This end point can be used to deactivate email notification for a single project, for all steps of a kind or at all.
  * 
  */
 @Path("/mails/disable")
+@Log4j
 public class Emails {
 
     /**
@@ -35,12 +42,13 @@ public class Emails {
      * @return
      */
     @Path("/all/{user}/{token}")
-    @Produces(MediaType.APPLICATION_XHTML_XML)
+    @Produces(MediaType.TEXT_HTML)
     @GET // should be POST, but must be GET to have a clickable link within an email
-    public Response deactivateEmailNotification(@PathParam("user") String userName, @PathParam("token") String token) {
+    public Response deactivateEmailNotification(@PathParam("user") String userName, @PathParam("token") String token,
+            @Context HttpServletRequest request, @Context HttpServletResponse response) {
 
         try {
-            userName =  URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
+            userName = URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e2) {
 
         }
@@ -48,15 +56,16 @@ public class Emails {
         tokenMap.put("purpose", "disablemails");
         tokenMap.put("type", "all");
         tokenMap.put("user", userName);
-        //        tokenMap.put("project", "");
-        //        tokenMap.put("stepname", "");
 
         try {
             if (!JwtHelper.validateToken(token, tokenMap)) {
-                return Response.status(404).build();
+                //                return Response.status(404).build();
+                return getErrorPage(Status.BAD_REQUEST, "invalid request, your url is worng or to old.");
             }
         } catch (ConfigurationException e1) {
-            return Response.status(500).build();
+            log.error(e1);
+            //            return Response.status(500).build();
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Configuration error, try again later.");
         }
 
         User user = UserManager.getUserByLogin(userName);
@@ -70,22 +79,30 @@ public class Emails {
         try {
             UserManager.saveUser(user);
         } catch (DAOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error(e);
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Save error, try again later.");
         }
 
-        return Response.ok().build();
+        String myJsfPage = "/uii/mailNotificationDisabled.xhtml";
+        String contextPath = request.getContextPath();
+        try {
+            response.sendRedirect(contextPath + myJsfPage);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return null;
+
     }
 
     @Path("/step/{user}/{step}/{token}")
-    @Produces(MediaType.APPLICATION_XHTML_XML)
+    @Produces(MediaType.TEXT_HTML)
     @GET // should be POST, but must be GET to have a clickable link within an email
     public Response deactivateEmailNotificationForStep(@PathParam("user") String userName, @PathParam("step") String step,
-            @PathParam("token") String token) {
+            @PathParam("token") String token, @Context HttpServletRequest request, @Context HttpServletResponse response) {
 
         try {
-            userName =  URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
-            step =  URLDecoder.decode(step, StandardCharsets.UTF_8.toString());
+            userName = URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
+            step = URLDecoder.decode(step, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e2) {
 
         }
@@ -98,10 +115,10 @@ public class Emails {
 
         try {
             if (!JwtHelper.validateToken(token, deactivateStepMap)) {
-                return Response.status(404).build();
+                return getErrorPage(Status.BAD_REQUEST, "invalid request, your url is worng or to old.");
             }
         } catch (ConfigurationException e1) {
-            return Response.status(500).build();
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Configuration error, try again later.");
         }
 
         User user = UserManager.getUserByLogin(userName);
@@ -117,22 +134,29 @@ public class Emails {
         try {
             UserManager.saveUser(user);
         } catch (DAOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error(e);
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Save error, try again later.");
         }
 
-        return Response.ok().build();
+        String myJsfPage = "/uii/mailNotificationDisabled.xhtml";
+        String contextPath = request.getContextPath();
+        try {
+            response.sendRedirect(contextPath + myJsfPage);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return null;
     }
 
     @Path("/project/{user}/{project}/{token}")
-    @Produces(MediaType.APPLICATION_XHTML_XML)
+    @Produces(MediaType.TEXT_HTML)
     @GET // should be POST, but must be GET to have a clickable link within an email
     public Response deactivateEmailNotificationForProject(@PathParam("user") String userName, @PathParam("project") String project,
-            @PathParam("token") String token) {
+            @PathParam("token") String token, @Context HttpServletRequest request, @Context HttpServletResponse response) {
 
         try {
-            userName =  URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
-            project =  URLDecoder.decode(project, StandardCharsets.UTF_8.toString());
+            userName = URLDecoder.decode(userName, StandardCharsets.UTF_8.toString());
+            project = URLDecoder.decode(project, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e2) {
 
         }
@@ -145,10 +169,10 @@ public class Emails {
 
         try {
             if (!JwtHelper.validateToken(token, deactivateProjectMap)) {
-                return Response.status(404).build();
+                return getErrorPage(Status.BAD_REQUEST, "invalid request, your url is worng or to old.");
             }
         } catch (ConfigurationException e1) {
-            return Response.status(500).build();
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Configuration error, try again later.");
         }
 
         User user = UserManager.getUserByLogin(userName);
@@ -164,11 +188,29 @@ public class Emails {
         try {
             UserManager.saveUser(user);
         } catch (DAOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error(e);
+            return getErrorPage(Status.INTERNAL_SERVER_ERROR, "Save error, try again later.");
         }
 
-        return Response.ok().build();
+        String myJsfPage = "/uii/mailNotificationDisabled.xhtml";
+        String contextPath = request.getContextPath();
+        try {
+            response.sendRedirect(contextPath + myJsfPage);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return null;
     }
 
+    private Response getErrorPage(Status status, String errorText) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<body>");
+        sb.append("<p>");
+        sb.append(errorText);
+        sb.append("</p>");
+        sb.append("</body>");
+        sb.append("</html>");
+        return Response.status(status).entity(sb.toString()).build();
+    }
 }
